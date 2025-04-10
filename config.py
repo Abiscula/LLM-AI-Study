ba2173af43a77a4a11b35664760cc5d9db411bac
Victor Abiscula <victorabiscula@live.com>
2025-04-10 18:37:06 -0300

    refactor: Realizada criação de funções para o config do model e do pipeline visando uma melhoraria do codigo.

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
 from dotenv import load_dotenv
+from utils.quantization import quantization
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -11,3 +15,40 @@ def load_config():
   return {
     'HF_TOKEN': hf_token
   }
+
+def create_ai_model():
+  model_id = 'microsoft/Phi-3-mini-4k-instruct'
+
+  config = load_config()
+  hf_token = config['HF_TOKEN']
+
+  if not hf_token:
+    pass
+
+  quantization_config = quantization()
+
+  model = AutoModelForCausalLM.from_pretrained(
+    pretrained_model_name_or_path = model_id, 
+    quantization_config = quantization_config,
+    token = hf_token,
+  )
+
+  tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+  return [model, tokenizer]
+
+def create_pipeline():
+  [model, tokenizer] = create_ai_model()
+
+  pipe = pipeline(
+    model = model,
+    tokenizer = tokenizer,
+    task = "text-generation",
+    temperature = 0.1,
+    max_new_tokens = 500,
+    do_sample = True,
+    repetition_penalty = 1.1,
+    return_full_text = False
+  )
+
+  return pipe
--- simple_model.py
+++ simple_model.py
@@ -1,7 +1,5 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
 import torch
-from config import load_config
-from utils.quantization import quantization
+from config import create_pipeline
 
 # Criação do template/prompt do usuário
 def user_prompt():
@@ -19,39 +17,9 @@ def user_prompt():
 def load_simple_model():
   # Garantindo a reprodutibilidade entre diversas execuções
   torch.random.manual_seed(40)
-
-  config = load_config()
-  hf_token = config['HF_TOKEN']
-
-  if not hf_token:
-    pass
-      
-  quantization_config = quantization()
-
-  model_id = 'microsoft/Phi-3-mini-4k-instruct'
-
-  model = AutoModelForCausalLM.from_pretrained(
-    pretrained_model_name_or_path=model_id, 
-    device_map="cuda",
-    torch_dtype="auto",
-    trust_remote_code=True,
-    attn_implementation="eager",
-    token=hf_token,
-    quantization_config=quantization_config
-  )
-
-  tokenizer = AutoTokenizer.from_pretrained(model_id)
-  
-  # Criando o pipeline de geração de texto
-  pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-  generations_args = {
-    "max_new_tokens": 500,
-    "return_full_text": False,
-    "temperature": 0.1,  # Varia de 0.1 até 0.9
-    "do_sample": True
-  }
-
+    
+  pipe = create_pipeline()
   template = user_prompt()
 
-  output = pipe(template, **generations_args)
+  output = pipe(template)
   print(output[0]['generated_text'])
