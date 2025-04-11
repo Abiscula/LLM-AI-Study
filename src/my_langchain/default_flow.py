1a42ffdc750fc800618b038377c841e9038f06bd
Victor Abiscula <victorabiscula@live.com>
2025-04-11 18:36:16 -0300

    feat: Criado novo fluxo com outputParser
    Neste exemplo foi implementado o StrOutputParser que irá converter a resposta em string

--- src/my_langchain/default_flow.py
+++ src/my_langchain/default_flow.py
@@ -1,8 +1,8 @@
 from langchain_huggingface import (
   HuggingFacePipeline
 )
-
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 from src.config import create_pipeline
 
@@ -15,6 +15,7 @@ def user_template():
 
   return template
 
+# Fluxo de criação de uma chain
 def chain_flow(llm, template):
   user_prompt = "Explique brevemente o conceito de {topic} de forma clara e objetiva." \
   "Escreva no máximo {length}"
@@ -23,15 +24,24 @@ def chain_flow(llm, template):
   prompt = PromptTemplate.from_template(template.format(sys_prompt, user_prompt))
   chain = prompt | llm
 
-  response = chain.invoke({"topic": "IA", "length": "1 frase"})
-  print(response)
+  return chain
+
+# converte a resposta para string
+def str_output_parser(chain):
+  chain_str = chain | StrOutputParser()
+  return chain_str
 
 def load_lang_chain():
   pipe = create_pipeline()
   llm = HuggingFacePipeline(pipeline = pipe)
   template = user_template()
 
-  chain_flow(llm, template)
+  chain = chain_flow(llm, template)
+
+  parsed_chain = str_output_parser(chain)
+
+  response = parsed_chain.invoke({"topic": "IA", "length": "1 frase"})
+  print(response)
 
 
   
