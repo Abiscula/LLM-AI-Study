61585637039597ce7b30217b77fdc44e935c6f6a
Victor Abiscula <victorabiscula@live.com>
2025-04-13 11:38:12 -0300

    feat: Criado toolkit com wikipedia_tool e date_tool

--- src/my_langchain/agent_flow.py
+++ src/my_langchain/agent_flow.py
@@ -9,21 +9,34 @@ from src.config import create_pipeline
 
 
 # Busca dados direto da wikipedia
-def load_wikipedia_tool():
+def wikipedia_tool():
   wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper(
     top_k_results=1, doc_content_chars_max=3000
   ))
-  wikipedia_tool = Tool(
+  tool = Tool(
     name='wikipedia', 
     description="You must never search for multiple concepts at a single step, " \
     "you need to search one at a time. When asked to compare two concepts for example, " \
     "search for each one individually.",
     func=wikipedia.run
   )
+  return tool
 
+# Função para obter o dia atual
+def current_day(*args, **kwargs):
+  from datetime import date
+
+  day = date.today()
+  day = day.strftime('%d/%m/%Y')
+  return day
+
+# Tool customizada para trabalhar com o dia atual
+def date_tool():
+  date = Tool(name="Day", func=current_day,
+              description="Use when you need to know the current date")
+  return date
 
 def load_agent():
   pipe = create_pipeline()
   llm = HuggingFacePipeline(pipeline = pipe)
-
-  load_wikipedia_tool()
+  tools = [wikipedia_tool(), date_tool()]
