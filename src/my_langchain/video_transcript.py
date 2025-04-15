5d81b825974b729a41a11f145e5d0c51721e861c
Victor Abiscula <victorabiscula@live.com>
2025-04-15 00:20:42 -0300

    feat:: adiciona suporte à transcrição automática de vídeos para pt-BR
    - Implementado carregamento de vídeos do YouTube com suporte a múltiplos idiomas
    - Transcrição forçada para português do Brasil mesmo quando o vídeo está em inglês

--- src/my_langchain/video_transcript.py
+++ src/my_langchain/video_transcript.py
@@ -15,13 +15,19 @@ def get_llama_llm():
   hf_token = config['HF_TOKEN']
   os.environ["HF_TOKEN"] = hf_token
 
-  llm = HuggingFaceEndpoint(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", temperature=0.1)
+  llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Meta-Llama-3-8B-Instruct", 
+    temperature=0.1,
+    task="text-generation"
+  )
   return llm
 
 def video_transcription():
+  video_url = 'https://www.youtube.com/watch?v=eXdVDhOGqoE'
   video_loader = YoutubeLoader.from_youtube_url(
-      'https://www.youtube.com/watch?v=1IyxRkNj9lU',
-      language=["pt", "pt-BR", "en"]
+      video_url,
+      language=["pt", "pt-BR", "en"],
+      translation='pt'
     )
   infos = video_loader.load()
 
@@ -32,7 +38,31 @@ def write_video_transcription(infos):
     for doc in infos:
       f.write(doc.page_content + "\n\n")
 
+def create_prompt():
+  system_prompt = "Você é um assistente virtual prestativo e deve responder a uma" \
+  "consulta com base na transcrição de um vídeo que será fornecida abaixo"
+
+  inputs = "Consulta: {consulta} \n Transcrição: {transcricao}"
+  user_prompt = "{}".format(inputs)
+
+  prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_prompt), ("user", user_prompt)]
+  )
+  return prompt_template
+
+def create_chain(prompt, llm):
+  chain = prompt | llm | StrOutputParser()
+  return chain
+
 def load_video_transcript():
-  llm = get_llama_llm()
   infos = video_transcription()
-  write_video_transcription(infos)
+  # write_video_transcription(infos)
+
+  llm = get_llama_llm()
+  prompt = create_prompt()
+  chain = create_chain(prompt, llm)
+  response = chain.invoke({
+    "transcricao": infos[0].page_content,
+    "consulta": "liste os temas desse video"
+  })
+  print(response)
